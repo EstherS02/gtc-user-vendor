2de3d4bfa421e90cc65f6dbbeeb8132e4dda9f62
Esther <estherpushpa04@gmail.com>
2018-05-05 12:12:11 +0530

    api for feature product done

--- server/api/product/product.controller.js
+++ server/api/product/product.controller.js
@@ -6,103 +6,103 @@ const model = require('../../sqldb/model-connect');
 export function index(req, res) {
 
 	model["VendorPlan"].find({
-		where: req.query
-	})
-	.then(function(row) {
-		if (row) {
-			model["PlanMarketplace"].find({
-				where: {
-					plan_id: row.plan_id
-				}
-			})
-			.then(function(row) {
-				if (row) {
+			where: req.query
+		})
+		.then(function(row) {
+			if (row) {
+				model["PlanMarketplace"].find({
+						where: {
+							plan_id: row.plan_id
+						}
+					})
+					.then(function(row) {
+						if (row) {
 
-					res.status(200).send(row);
-					return;
-				} else {
-					res.status(404).send("Not Found");
-					return;
-				}
-			})
-			.catch(function(error) {
-				if (error) {
-					res.status(500).send(error);
-					return;
-				}
-			})
-		} else {
-			res.status(404).send("Not Found");
-			return;
-		}
-	})
-	.catch(function(error) {
-		if (error) {
-			res.status(500).send(error);
-			return;
-		}
-	})
+							res.status(200).send(row);
+							return;
+						} else {
+							res.status(404).send("Not Found");
+							return;
+						}
+					})
+					.catch(function(error) {
+						if (error) {
+							res.status(500).send(error);
+							return;
+						}
+					})
+			} else {
+				res.status(404).send("Not Found");
+				return;
+			}
+		})
+		.catch(function(error) {
+			if (error) {
+				res.status(500).send(error);
+				return;
+			}
+		})
 }
 
 export function featureMany(req, res) {
 	const ids = req.body.ids;
-	console.log(ids);
-	// model[req.endpoint].update({
-	// 	status: status["DELETED"],
-	// 	deleted_at: new Date()
-	// }, {
-	// 	where: {
-	// 		id: ids
-	// 	}
-	// }).then(function(rows) {
-	// 	if (rows[0] > 0) {
-	// 		res.status(200).send(rows);
-	// 		return;
-	// 	} else {
-	// 		res.status(404).send("Unable to delete users");
-	// 		return;
-	// 	}
-	// }).catch(function(error) {
-	// 	console.log('Error:::', error);
-	// 	res.status(500).send("Internal server error");
-	// 	return;
-	// })
+	console.log("requestedIds", ids.length);
+	var arr = [];
+
+	for (var i = 0; i <= ids.length - 1; i++) {
+		var obj = {};
+		obj['product_id'] = ids[i];
+		obj['status'] = 1;
+		obj['start_date'] = new Date();
+		obj['created_on'] = new Date();
+		arr.push(obj);
+	}
+	console.log("array", arr)
+
+	model["FeaturedProduct"].bulkCreate(arr)
+		.then(function(row) {
+			res.status(201).send("Created");
+			return;
+		}).catch(function(error) {
+			if (error) {
+				res.status(500).send(error);
+				return;
+			}
+		});
 }
 
 export function featureOne(req, res) {
 
-	console.log("requested parameter id",req.params.id);
+	console.log("requested parameter id", req.params.id);
 
 	model["Product"].findById(req.params.id)
-	.then(function(row) {
+		.then(function(row) {
+			if (row) {
 
-		if (row) {
-			var obj= {};
+				var obj = {};
+				obj['product_id'] = row.id;
+				obj['status'] = 1;
+				obj['start_date'] = new Date();
+				obj['created_on'] = new Date();
 
-			obj['product_id']= row.id;
-			obj['status']=1;
-			obj['start_date']= new Date();
-			obj['created_on'] = new Date();
-
-			model["FeaturedProduct"].create(obj)
-			.then(function(row) {
-				res.status(201).send("Created");
+				model["FeaturedProduct"].create(obj)
+					.then(function(row) {
+						res.status(201).send("Created");
+						return;
+					})
+					.catch(function(error) {
+						if (error) {
+							res.status(500).send(error);
+							return;
+						}
+					});
+			} else {
+				res.status(404).send("Not found");
 				return;
-			})
-			.catch(function(error) {
-				if (error) {
-					res.status(500).send(error);
-					return;
-				}
-			});
-		} else {
-			res.status(404).send("Not found");
+			}
+		}).catch(function(error) {
+			console.log('Error:::', error);
+			res.status(500).send("Internal server error");
 			return;
-		}
-	}).catch(function(error) {
-		console.log('Error:::', error);
-		res.status(500).send("Internal server error");
-		return;
-	})
+		})
 }
-
