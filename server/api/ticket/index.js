d29098276222ed0a2649bc3d8802ca38177f7eac
mchandirasekar <mchandru@ibytecode.com>
2018-05-15 23:45:24 +0530

    created ticket update method

--- server/api/ticket/index.js
+++ server/api/ticket/index.js
@@ -6,5 +6,6 @@ var auth = require('../../auth/auth.service');
 var controller = require('./ticket.controller');
 
 router.post('/', auth.isAuthenticated(), controller.create);
+router.post('/:id', auth.isAuthenticated(), controller.update);
 
 module.exports = router;
