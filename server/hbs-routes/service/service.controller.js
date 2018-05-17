e027c1c0f865051d4431541ff465d94b693bce46
Esther <estherpushpa04@gmail.com>
2018-05-17 17:24:06 +0530

    Startcount and Titlecase helper added

--- server/hbs-routes/service/service.controller.js
+++ server/hbs-routes/service/service.controller.js
@@ -5,7 +5,8 @@ const model = require('../../sqldb/model-connect');
 const reference = require('../../config/model-reference');
 const status = require('../../config/status');
 const service = require('../../api/service');
-var async = require('async');
+
+const async = require('async');
 import series from 'async/series';
 
 export function Service(req, res) {
