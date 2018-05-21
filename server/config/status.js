7c6aebf9253c322cd5912c0b0e83ab541792db79
Esther <estherpushpa04@gmail.com>
2018-05-21 16:03:49 +0530

    product findall api integration

--- server/config/status.js
+++ server/config/status.js
@@ -4,5 +4,7 @@ module.exports = {
 	"DELETED": 3,
 	"OPEN": 4,
 	"HOLD": 5,
-	"CLOSED": 6
+	"CLOSED": 6,
+	"SUSPENDED":7,
+	"SOLD OUT":8
 };
--- server/lib/helpers.js
+++ server/lib/helpers.js
@@ -4,7 +4,7 @@ import Handlebars from 'handlebars';
 Handlebars.registerHelper('starCount', function (rating) {
 
     var rating = Math.trunc(rating);
-    
+
     var colored = "";
     var colorless = "";
     var tag1 = "<i class=" + '"fa fa-star"' + " aria-hidden=" + '"true"' + " style=" + '"color: #CDBE29;"></i>';
@@ -25,28 +25,47 @@ Handlebars.registerHelper('convertUpperCase', function (msg) {
 });
 
 Handlebars.registerHelper('Titlecase', function (str) {
-    return str.replace(/\w\S*/g, function(txt){
+    return str.replace(/\w\S*/g, function (txt) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });;
 });
 
 
-Handlebars.registerHelper('DisplayJSON', function(context, options) {
-    if(!context)
+Handlebars.registerHelper('DisplayJSON', function (context, options) {
+    if (!context)
         return 'null';
 
     return JSON.stringify(context);
 });
 
-Handlebars.registerHelper('toURL', function(text, options) {
-    if(!text)
+Handlebars.registerHelper('toURL', function (text, options) {
+    if (!text)
         return 'null';
 
     return text.split(' ').join('-');
 });
 
+// Handlebars.registerHelper("prettifyDate", function(timestamp) {
+//     return new Date(timestamp).toDateString();
+// });
 
+Handlebars.registerHelper("prettifyDate", function (timestamp) {
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+    var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = monthNames[d.getMonth()]
+    var curr_year = d.getFullYear();
+    return curr_month + ' ' + curr_date + ', ' + curr_year;
+});
 
+Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+    if(v1 === v2) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
 
 
 
