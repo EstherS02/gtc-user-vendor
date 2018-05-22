810b36533d50c8c8a4322e0769c9c19fd96cac19
Esther <estherpushpa04@gmail.com>
2018-05-22 18:43:33 +0530

    directory api configuration

--- server/hbs-routes/directory/directory.controller.js
+++ server/hbs-routes/directory/directory.controller.js
@@ -4,10 +4,45 @@ const config = require('../../config/environment');
 const model = require('../../sqldb/model-connect');
 const reference = require('../../config/model-reference');
 const status = require('../../config/status');
+const service = require('../../api/service');
+const async = require('async');
+import series from 'async/series';
 
 
 export function directory(req, res) {
-	res.render('directory', {
-        title: 'Global Trade Connect'
+    var field = "id";
+    var order = "asc";
+
+    async.series({
+        category: function (callback) {
+
+            service.findRows('Category', { status: 1 }, 0, 10, field, order)
+                .then(function (category) {
+                    return callback(null, category.rows);
+
+                }).catch(function (error) {
+                    console.log('Error :::', error);
+                    return callback(null);
+                });
+        },
+        subCategory: function (callback) {
+
+            service.findRows('SubCategory', { status: 1 }, null, null, field, order)
+                .then(function (subCategory) {
+                    console.log("subCategory.rows",subCategory.rows)
+                    return callback(null, subCategory.rows);
+
+                }).catch(function (error) {
+                    console.log('Error :::', error);
+                    return callback(null);
+                });
+        }
+    }, function (err, results) {
+        if (!err) {
+             res.render('directory', results);
+        }
+        else {
+            res.render('directory', err);
+        }
     });
 }
--- server/routes.js
+++ server/routes.js
@@ -57,8 +57,6 @@ export default function(app) {
   });
 
 
-
-
 /*   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
     .get(errors[404]); 
