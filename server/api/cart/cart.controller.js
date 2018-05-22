c2ba88c5469ebbce626bbe13de5b98505aa0b5a9
svdevan <svdevan@ibytecode.com>
2018-05-22 14:59:23 +0530

    add to cart endpoint added

--- server/api/cart/cart.controller.js
+++ server/api/cart/cart.controller.js
@@ -3,6 +3,7 @@
 const config = require('../../config/environment');
 const model = require('../../sqldb/model-connect');
 const service = require('../service');
+const status = require('../../config/status')
 
 export function addToCart(req, res){
 
@@ -17,64 +18,133 @@ export function addToCart(req, res){
 			if (productResult) {
 				productResult = plainTextResponse(productResult);
 
-				if(order_qty > productResult.quantity_available){
-					res.send("You are ordering more");
-				}else{
+				if(parseInt(productResult.quantity_available) == 0){
+					
+					res.status(400).json({
+						message : "SOLD_OUT",
+						message_details : "OOPS ! This product sold out, seller has no more left"
+					});
+
+				} else if(order_qty > productResult.quantity_available){
+					
+					res.status(400).json({
+						message : "EXCEEDING_AVAILABLITY",
+						message_details : "This seller have only " + productResult.quantity_available + "Items of this product",
+						available_state: productResult.quantity_available				 
+					});
+					
+				} else{
 					var cartSearchObj = {}
 					cartSearchObj['user_id'] = user_id;
-					cartSearchObj['product_id'] = 16 //productResult.id;
-
-					console.log(cartSearchObj)
+					cartSearchObj['product_id'] = productResult.id;
 
 					model["Cart"].findOne({
 							where: cartSearchObj
 						}).then(function(cartResult) {
 							if (cartResult) {
-								//Item already in cart?
 								cartResult = plainTextResponse(cartResult);
-								console.log(cartResult);
 
-								var cartUpdateObj = {};
-								cartUpdateObj['quantity'] = cartResult.quantity + order_qty;
+								var checkCartUpdateQuantity = productResult.quantity_available - cartResult.quantity;
 
-								model["Cart"].update(cartUpdateObj, {
-									where: {
-										id: cartResult.id
-									}
-								}).then(function(updatedCartResult) {
-									if (updatedCartResult) {
-										console.log("successfully updated", updatedCartResult)
-									} else {
-										console.log("unable to add to the cart")
-									}
-								}).catch(function(error) {
-									console.log('Error:::', error);
-									return res.status(500).send("Internal server error");
-								})
+								if(order_qty > checkCartUpdateQuantity){
+									
+									res.status(400).json({
+										message : "EXCEEDING_AVAILABLITY",
+										message_details : "Already you have " + cartResult.quantity + " Quantities of this Product in your cart, This seller only have " + checkCartUpdateQuantity + " Remaining",
+										available_state: checkCartUpdateQuantity
+									});
+
+								}else{
+									var cartUpdateObj = {};
+									cartUpdateObj['quantity'] = cartResult.quantity + order_qty;
+									cartUpdateObj['last_updated_by'] = "devan user";
+									cartUpdateObj['last_updated_on'] = new Date();
+
+									model["Cart"].update(cartUpdateObj, {
+										where: {
+											id: cartResult.id
+										}
+									}).then(function(updatedCartResult) {
+										if (updatedCartResult) {
+											res.status(200).json({
+												message : "SUCCESS",
+												message_details : "Product already in cart, Product quantity updated to " + cartUpdateObj['quantity']
+											});
+										} else {
+											return res.status(500).json({
+												message : "ERROR",
+												message_details : "Internal Server Error, Unable to add to cart.",
+											});
+										}
+									}).catch(function(error) {
+										console.log('Error:::', error);
+										return res.status(500).json({
+											message : "ERROR",
+											message_details : "Internal Server Error, Unable to add to cart.",
+										});
+									})
+								}
 								
 							} else {
-								console.log("New Item to cart")
+		
+							 	var createCartObj = {};
+								createCartObj["user_id"] = user_id;
+								createCartObj["product_id"] = productResult.id;
+								createCartObj["quantity"] = order_qty;
+								createCartObj["status"] = status.ACTIVE;
+								createCartObj["created_by"] = "devan user";
+								createCartObj["created_on"] = new Date();
+
+								model["Cart"].create(createCartObj)
+								.then(function(createdCartResult) {
+									if (createdCartResult) {
+										res.status(200).json({
+											message : "SUCCESS",
+											message_details : "Product successfully added to cart"
+										});
+									} else {
+										return res.status(500).json({
+											message : "ERROR",
+											message_details : "Internal Server Error, Unable to add to cart.",
+										});
+									}
+								}).catch(function(error) {
+										console.log('Error:::', error);
+										return res.status(500).json({
+											message : "ERROR",
+											message_details : "Internal Server Error, Unable to add to cart.",
+										});
+								});
 								
 							}
 					}).catch(function(error) {
 						console.log('Error:::', error);
-						res.status(500).send("Internal server error");
-						return;
+						return res.status(500).json({
+							message : "ERROR",
+							message_details : "Internal Server Error, Unable to add to cart.",
+						});
 					})
 					
 				}
 
 			} else {
-				res.status(404).send("Not found");
-				return;
+				return res.status(404).json({
+					message : "NOT_FOUND",
+					message_details : "Product not found",
+				});
 			}
 		}).catch(function(error) {
 			console.log('Error:::', error);
-			res.status(500).send("Internal server error");
-			return;
+			return res.status(500).json({
+				message : "ERROR",
+				message_details : "Internal Server Error, Unable to add to cart.",
+			});
 		})
 	}else{
-		res.status(500).send("Please Enter the Quantity you want");
+		return res.status(500).json({
+			message : "NO_QUANTITY",
+			message_details : "Please Enter the Quantity you want",
+		});
 	}
 
 }
