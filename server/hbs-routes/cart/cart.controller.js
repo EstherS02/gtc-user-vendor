a93663398e43a58bbd465a54a6ae0014fcbdf079
svdevan <svdevan@ibytecode.com>
2018-05-24 19:04:16 +0530

    Cart Page

--- server/hbs-routes/cart/cart.controller.js
+++ server/hbs-routes/cart/cart.controller.js
@@ -13,31 +13,68 @@ export function cart(req, res) {
     let user_id = 63;
 
     async.series({
-        one: function(cb) {
+        cartItems: function(cb) {
             
-        var queryObj = {};
-        queryObj['user_id'] = user_id;
+            var queryObj = {};
+            queryObj['user_id'] = user_id;
 
-        service.findRows('Cart', queryObj, null, null, "id", "asc")
-          .then(function (rows) {
-            return cb(null, rows);
-          }).catch(function (error) {
-              console.log(error)
-            return cb(error);
-          });
+            model['Cart'].findAndCountAll({
+                where: queryObj,
+                include: [
+                    { model: model["User"] }, 
+                    { model: model["Product"],
+                    include : [
+                               { model : model['Vendor'] },
+                               { model: model['Marketplace'] },
+                               { model: model['MarketplaceType'] }
+                            ]
+                        }],
+                offset: null,
+                limit: null,
+                order: [
+                    ["created_on", "asc"]
+                ]
+            }).then(function(data) {
+                return cb(null, data)
+            }).catch(function(error) {
+                return cb(error);
+            });
             
-        }
+        },
+        marketPlace: function(cb) {
+
+            model['Marketplace'].findAndCountAll({
+                where: {},
+                offset: null,
+                limit: null,
+                order: [
+                    ["created_on", "asc"]
+                ]
+            }).then(function(marketPlaceData) {
+                return cb(null, marketPlaceData)
+            }).catch(function(error) {
+                return cb(error);
+            });
+            
+        }        
     }, function(err, results) {
         if(!err){
-            return res.status(200).send(results);
+             return res.status(200).render('cart', {
+                title : "Global Trade Connect",
+                cartItems: results.cartItems.rows,
+                cartItemsCount: results.cartItems.count,
+                marketPlaces: results.marketPlace.rows
+            });
+            /* return res.status(200).send({
+                title : "Global Trade Connect",
+                cartItems: results.cartItems.rows,
+                cartItemsCount: results.cartItems.count,
+                marketPlaces: results.marketPlace.rows
+            }); */
         }else{
-            console.log('Error :::', error);
-			return res.status(500).send("Internal server error");
+            console.log(err)
+			return res.status(500).render(err);
         }
-        /* res.render('cart', {
-            title: 'Global Trade Connect',
-            results: results.one
-        }); */
     });
 
 }
