8ca37383ddda154151bc83582040c2296076df7d
MChandru <csomethingr@gmail.com>
2018-06-02 00:28:24 +0530

    product single view page

--- server/hbs-routes/wholesale/wholesale.controller.js
+++ server/hbs-routes/wholesale/wholesale.controller.js
@@ -10,6 +10,14 @@ const service = require('../../api/service');
 const async = require('async');
 import series from 'async/series';
 
+export function wholeSaleProductView(req, res) {
+    console.log('productSlugName', req.params.productSlugName);
+    console.log('marketPlaceType', req.params.marketPlaceType);
+    res.render('product-view', {
+        title: "Global Trade Connect"
+    });
+}
+
 export function wholesale(req, res) {
     var productModel = "ProductSalesRating";
     var featuredProductModel = "FeaturedproductSalesRating";
@@ -20,7 +28,7 @@ export function wholesale(req, res) {
     var queryObj = {};
     var LoggedInUser = {}
 
-    if(req.gtcGlobalUserObj && req.gtcGlobalUserObj.isAvailable)
+    if (req.gtcGlobalUserObj && req.gtcGlobalUserObj.isAvailable)
         LoggedInUser = req.gtcGlobalUserObj;
 
     offset = 0;
@@ -31,104 +39,104 @@ export function wholesale(req, res) {
     queryObj['status'] = status["ACTIVE"];
 
     async.series({
-        wantToSell: function (callback) {
+        wantToSell: function(callback) {
             queryObj['marketplace_type'] = 'Want To Sell';
             service.findRows(productModel, queryObj, offset, limit, field, order)
-                .then(function (wantToSell) {
+                .then(function(wantToSell) {
                     return callback(null, wantToSell.rows);
 
-                }).catch(function (error) {
+                }).catch(function(error) {
                     console.log('Error :::', error);
                     return callback(null);
                 });
         },
-        wantToBuy: function (callback) {
+        wantToBuy: function(callback) {
             queryObj['marketplace_type'] = 'Want To Buy';
             service.findRows(productModel, queryObj, offset, limit, field, order)
-                .then(function (wantToBuy) {
+                .then(function(wantToBuy) {
                     return callback(null, wantToBuy.rows);
 
-                }).catch(function (error) {
+                }).catch(function(error) {
                     console.log('Error :::', error);
                     return callback(null);
                 });
         },
-        wantToTrade: function (callback) {
+        wantToTrade: function(callback) {
             queryObj['marketplace_type'] = 'Want To Trade';
             service.findRows(productModel, queryObj, offset, limit, field, order)
-                .then(function (wantToTrade) {
+                .then(function(wantToTrade) {
                     return callback(null, wantToTrade.rows);
 
-                }).catch(function (error) {
+                }).catch(function(error) {
                     console.log('Error :::', error);
                     return callback(null);
                 });
         },
-        requestForQuote: function (callback) {
+        requestForQuote: function(callback) {
             queryObj['marketplace_type'] = 'Request For Quote';
             service.findRows(productModel, queryObj, offset, limit, field, order)
-                .then(function (requestForQuote) {
+                .then(function(requestForQuote) {
                     return callback(null, requestForQuote.rows);
 
-                }).catch(function (error) {
+                }).catch(function(error) {
                     console.log('Error :::', error);
                     return callback(null);
                 });
         },
-        featuredProducts: function (callback) {
+        featuredProducts: function(callback) {
             limit = null;
             delete queryObj['marketplace_type'];
             queryObj['position'] = position.WholesaleLanding;
             queryObj['marketplace'] = 'Private Wholesale Marketplace';
             service.findRows(featuredProductModel, queryObj, offset, limit, field, order)
-                .then(function (featuredProducts) {
+                .then(function(featuredProducts) {
                     return callback(null, featuredProducts.rows);
 
-                }).catch(function (error) {
+                }).catch(function(error) {
                     console.log('Error :::', error);
                     return callback(null);
                 });
         },
-        category: function (callback) {
+        category: function(callback) {
             limit = null;
             delete queryObj['marketplace'];
             delete queryObj['position'];
             service.findRows(categoryModel, queryObj, offset, limit, field, order)
-                .then(function (category) {
+                .then(function(category) {
                     return callback(null, category.rows);
 
-                }).catch(function (error) {
+                }).catch(function(error) {
                     console.log('Error :::', error);
                     return callback(null);
                 });
         },
-        country: function (callback) {
+        country: function(callback) {
             limit = null;
             service.findRows(countryModel, queryObj, offset, limit, field, order)
-                .then(function (country) {
+                .then(function(country) {
                     return callback(null, country.rows);
 
-                }).catch(function (error) {
+                }).catch(function(error) {
                     console.log('Error :::', error);
                     return callback(null);
                 });
         },
-         wholesalers: function (callback) {
+        wholesalers: function(callback) {
             delete queryObj['marketplace'];
             queryObj['type'] = 'Private Wholesale Marketplace';
             field = 'sales_count';
             order = 'desc';
             limit = 6;
             service.findRows(vendorModel, queryObj, offset, limit, field, order)
-                .then(function (wholesalers) {
+                .then(function(wholesalers) {
                     return callback(null, wholesalers.rows);
 
-                }).catch(function (error) {
+                }).catch(function(error) {
                     console.log('Error :::', error);
                     return callback(null);
                 });
         },
-    }, function (err, results) {
+    }, function(err, results) {
         if (!err) {
             res.render('wholesale', {
                 title: "Global Trade Connect",
@@ -142,8 +150,7 @@ export function wholesale(req, res) {
                 country: results.country,
                 LoggedInUser: LoggedInUser
             });
-        }
-        else {
+        } else {
             res.render('wholesale', err);
         }
     });
