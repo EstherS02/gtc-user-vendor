6f16ae08f3522bf442aa70f7462d846ac9f9c381
svdevan <svdevan@ibytecode.com>
2018-06-07 20:06:06 +0530

    star rating added

--- server/hbs-routes/product-view/index.js
+++ server/hbs-routes/product-view/index.js
@@ -4,13 +4,14 @@ var express = require('express');
 var router = express.Router();
 var middleware = require('../../middleware');
 var permission = require('../../config/permission');
+var globalUser = require('../../auth/global-user-obj');
 
 
 /* Handlebars routes */
 var controller = require('./product-view.controller');
 
-router.get('/:productSlugName', controller.GetProductDetails);
-router.get('/:product_slug/:product_id', controller.productView);
+router.get('/:product_slug/:product_id', globalUser.isGlobalObj(), controller.GetProductDetails);
+//router.get('/:product_slug/:product_id', controller.productView);
 
 
 module.exports = router;
