e19a9aa8985ebaa6bfb4bb144a964ea7d4e04b75
rkesavan <kesavan@gmail.com>
2018-06-23 18:19:12 +0530

    add coupon

--- server/api/coupon/coupon.controller.js
+++ server/api/coupon/coupon.controller.js
@@ -7,6 +7,7 @@ const config = require('../../config/environment');
 const model = require('../../sqldb/model-connect');
 const status = require('../../config/status');
 const service = require('../service');
+var async = require('async');
 
 export function updateCoupon(req, res) {
 	var modelName = 'Coupon';
@@ -140,4 +141,85 @@ export function updateCategoryCoupon(req, res) {
 		}
 		return;
 	});
+}
+
+export function saveCoupon(req, res) {
+	var modelName = 'Coupon';
+
+	var newCoupon = {};
+	newCoupon.coupon_name 	 		= req.body.coupon_name;
+	newCoupon.code 			 		= req.body.code;
+	newCoupon.discount_type  		= req.body.discount_type;
+	newCoupon.discount_value 		= req.body.discount_value;
+	newCoupon.expiry_date 	 		= req.body.expiry_date;
+	newCoupon.minimum_spend  		= req.body.minimum_spend;
+	newCoupon.maximum_spend  		= req.body.maximum_spend;
+	newCoupon.individual_use_only 	= req.body.individual_use_only;
+	newCoupon.excluse_sale_item 	= req.body.excluse_sale_item;
+	newCoupon.status 				= 1;
+	newCoupon.vendor_id 			= req.body.vendor_id;
+	newCoupon.usage_limit 			= req.body.usage_limit;
+	newCoupon.limit_usage_to_x_items= req.body.limit_usage_to_x_items;
+	newCoupon.usage_limit_per_user 	= req.body.usage_limit_per_user;
+	newCoupon.publish_date 			= req.body.publish_date;
+	newCoupon.created_on 			= new Date();
+
+	var coupons = {};
+	coupons.product 			= JSON.parse(req.body.couponProducts);
+	coupons.excludeProduct 		= JSON.parse(req.body.couponExcludeProducts);
+	coupons.categories 			= JSON.parse(req.body.couponCategories);
+	coupons.excludeCategories	= JSON.parse(req.body.couponExcludeCategories);
+
+	service.createRow(modelName, newCoupon)
+		.then(function (result) {
+            if(result) {
+				var coupon_id = result.id;
+				var coupon_data = {};
+
+				coupon_data.product = {
+					modelName : "CouponProduct",
+					data : []
+				};
+				coupon_data.excludeProduct = {
+					modelName : "CouponExcludedProduct",
+					data : []
+				};
+				coupon_data.categories = {
+					modelName : "CouponCategory",
+					data : []
+				};
+				coupon_data.excludeCategories = {
+					modelName : "CouponExcludedCategory",
+					data : []
+				};
+				for(let key in coupons) {
+					let arr_data = coupons[key];
+					let key_name = 'category_id';
+					if(
+						coupon_data[key].modelName == 'CouponProduct' || 
+						coupon_data[key].modelName == 'CouponExcludedProduct'
+					) {
+						key_name = 'product_id'
+					}
+					arr_data.forEach((id) => {
+						let obj = {};
+						obj['coupon_id'] = coupon_id;
+						obj[key_name] = id;
+						obj['status'] = 1;
+						coupon_data[key].data.push(obj);
+					});
+				};
+
+				for(let key in coupon_data) {
+					console.log(key);
+					service.createBulkRow(coupon_data[key].modelName, coupon_data[key].data).then(function(response, err) {
+						return;
+					});
+				}
+				res.status(200).send("success");
+            }
+		}).catch(function (error) {
+			console.log('Error :::', error);
+            res.status(500).send("Internal server error");
+		});
 }
