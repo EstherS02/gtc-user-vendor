b773d20da1dec40ef3acd042df05f87ceb2a7a45
rsumithra <sumiraja28@gmail.com>
2018-06-27 11:07:04 +0530

    sumithra:add wishlist

--- server/routes.js
+++ server/routes.js
@@ -21,6 +21,7 @@ export default function(app) {
   app.use('/api/appclients', require('./api/appclients'));
   app.use('/api/admin', require('./api/admin'));
   app.use('/api/product', require('./api/product'));
+   app.use('/api/product-view', require('./api/product-view'));
   app.use('/api/users', require('./api/users'));
   app.use('/api/tickets', require('./api/ticket'));
   app.use('/api/ticket-threads', require('./api/ticket-thread'));
