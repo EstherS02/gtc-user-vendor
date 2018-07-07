6216d8136053aea1e373bcc7d1eea3f16d4d7756
svdevan <svdevan@ibytecode.com>
2018-07-07 19:48:27 +0530

    New layout added for menus

--- server/lib/helpers.js
+++ server/lib/helpers.js
@@ -34,9 +34,14 @@ Handlebars.registerHelper('convertUpperCase', function(msg) {
 });
 
 Handlebars.registerHelper('Titlecase', function(str) {
-    return str.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });;
+    if(str){
+        return str.replace(/\w\S*/g, function(txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    }else{
+        return '';
+    }
+    
 });
 
 
