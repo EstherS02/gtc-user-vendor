37880b192fe148be23f2a226d084fe442d7e3649
rsumithra <sumiraja28@gmail.com>
2018-07-20 13:45:37 +0530

    sumithra:vendor navbar changes

--- server/hbs-routes/vendorPages/services/services.controller.js
+++ server/hbs-routes/vendorPages/services/services.controller.js
@@ -29,19 +29,12 @@ export function vendorServices(req, res) {
 	var vendor_id = req.params.id;
 	queryObj['marketplace_id'] = marketplace['SERVICE'];
 	queryURI['marketplace_id'] = marketplace['SERVICE'];
+	queryObj['status'] = status['ACTIVE'];
 	queryObj['vendor_id'] = vendor_id;
-	// var vevndorIncludeArr = [{
-	// 	model:model['Country']
 
-	// }]
 
 	var queryPaginationObj = {};
-	// var queryURI = {};
 
-	// offset = 0;
-	// limit = 18;
-	// field = "created_on";
-	// order = 0;
 	offset = req.query.offset ? parseInt(req.query.offset) : 0;
 	queryPaginationObj['offset'] = offset;
 	delete req.query.offset;
@@ -89,6 +82,21 @@ export function vendorServices(req, res) {
 					vendor_verified_status: status['ACTIVE']
 				},
 				required:false
+
+			}, {
+				model: model['VendorFollower'],
+				where: {
+					user_id: req.user.id,
+					status: 1
+				},
+				required: false
+			},{
+				model: model['VendorRating'],
+				attributes: [
+					[sequelize.fn('AVG', sequelize.col('VendorRatings.rating')), 'rating']
+				],
+				group: ['VendorRating.vendor_id'],
+				required: false,
 			}];
 			service.findIdRow('Vendor', vendor_id, vendorIncludeArr)
 				.then(function(response) {
@@ -100,26 +108,43 @@ export function vendorServices(req, res) {
 				});
 		},
 		categories: function(callback) {
+			var includeArr = [];
+			const categoryOffset = 0;
+			const categoryLimit = null;
+			const categoryField = "id";
+			const categoryOrder = "asc";
+			const categoryQueryObj = {};
+
+			categoryQueryObj['status'] = status["ACTIVE"];
+
+			service.findAllRows(categoryModel, includeArr, categoryQueryObj, categoryOffset, categoryLimit, categoryField, categoryOrder)
+				.then(function(category) {
+					var categories = category.rows;
+					bottomCategory['left'] = categories.slice(0, 8);
+					bottomCategory['right'] = categories.slice(8, 16);
+					return callback(null, category.rows);
+				}).catch(function(error) {
+					console.log('Error :::', error);
+					return callback(null);
+				});
+		},
+		categoriesWithCount: function(callback) {
 			var result = {};
 			var categoryQueryObj = {};
 			var productCountQueryParames = {};
 
 			categoryQueryObj['status'] = status["ACTIVE"];
-			productCountQueryParames['marketplace_id'] = marketplace['LIFESTYLE'];
 			productCountQueryParames['status'] = status["ACTIVE"];
 			productCountQueryParames['vendor_id'] = vendor_id;
+			productCountQueryParames['marketplace_id'] = marketplace['SERVICE'];
 			service.getCategory(categoryQueryObj, productCountQueryParames)
 				.then(function(response) {
-					var categories = response.rows;
-					bottomCategory['left'] = categories.slice(0, 8);
-					bottomCategory['right'] = categories.slice(8, 16);
 					return callback(null, response);
-
 				}).catch(function(error) {
 					console.log('Error :::', error);
 					return callback(null);
 				});
-		}
+		},
 	}, function(err, results) {
 		// console.log(JSON.stringify(results.VendorDetail));
 		queryPaginationObj['maxSize'] = 5;
@@ -135,7 +160,7 @@ export function vendorServices(req, res) {
 				queryURI: queryURI,
 				page:page,
 				categories: results.categories.rows,
-				category: results.categories,
+				categoriesWithCount: results.categoriesWithCount,
 				bottomCategory: bottomCategory,
 				LoggedInUser: LoggedInUser,
 				selectedPage: 'services'
