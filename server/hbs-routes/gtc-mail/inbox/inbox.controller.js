03c46a4ff76242d450f7187a721fe07ca27cd67d
Esther <estherpushpa04@gmail.com>
2018-08-11 15:24:00 +0530

    gtc mail sent inbox draft view pages functionality

--- server/hbs-routes/gtc-mail/inbox/inbox.controller.js
+++ server/hbs-routes/gtc-mail/inbox/inbox.controller.js
@@ -11,60 +11,119 @@ const statusCode = require('../../../config/status');
 const service = require('../../../api/service');
 const populate = require('../../../utilities/populate');
 const vendorPlan = require('../../../config/gtc-plan');
+const mailStatus = require('../../../config/mail-status');
 
 export function inbox(req, res) {
 	var LoggedInUser = {};
 	var bottomCategory = {};
-	
+	var offset, limit, field, order, page, includeArray=[];
+
+	offset = 0;
+	limit = null;
+	field = "id";
+	order = "asc";
+	var mailModel = 'UserMail';
+
+	offset = req.query.offset ? parseInt(req.query.offset) : 0;
+	delete req.query.offset;
+	limit = req.query.limit ? parseInt(req.query.limit) : config.paginationLimit;
+	delete req.query.limit;
+
+	page = req.query.page ? parseInt(req.query.page) : 1;
+	delete req.query.page;
+
+	offset = (page - 1) * limit;
+
+
 	if (req.user)
 		LoggedInUser = req.user;
 
-	let user_id = LoggedInUser.id;
+	var user_id = LoggedInUser.id;
+
+	var queryObj = {
+		user_id : user_id,
+		'$or':[
+			{ mail_status: mailStatus["READ"]},
+			{ mail_status: mailStatus["UNREAD"] }	
+		],
+		status : statusCode["ACTIVE"],
+	};
+
 	async.series({
-		cartCounts: function(callback) {
-            service.cartHeader(LoggedInUser).then(function(response) {
-                return callback(null, response);
-            }).catch(function(error) {
-                console.log('Error :::', error);
-                return callback(null);
-            });
-        },
-			categories: function(callback) {
-				var includeArr = [];
-				const categoryOffset = 0;
-				const categoryLimit = null;
-				const categoryField = "id";
-				const categoryOrder = "asc";
-				var categoryModel = "Category";
-				const categoryQueryObj = {};
-
-				categoryQueryObj['status'] = statusCode["ACTIVE"];
-
-				service.findAllRows(categoryModel, includeArr, categoryQueryObj, categoryOffset, categoryLimit, categoryField, categoryOrder)
-					.then(function(category) {
-						var categories = category.rows;
-						bottomCategory['left'] = categories.slice(0, 8);
-						bottomCategory['right'] = categories.slice(8, 16);
-						return callback(null, category.rows);
-					}).catch(function(error) {
-						console.log('Error :::', error);
-						return callback(null);
-					});
-			}
+		cartCounts: function (callback) {
+			service.cartHeader(LoggedInUser).then(function (response) {
+				return callback(null, response);
+			}).catch(function (error) {
+				console.log('Error :::', error);
+				return callback(null);
+			});
 		},
-		function(err, results) {
+		categories: function (callback) {
+			var includeArr = [];
+			const categoryOffset = 0;
+			const categoryLimit = null;
+			const categoryField = "id";
+			const categoryOrder = "asc";
+			var categoryModel = "Category";
+			const categoryQueryObj = {};
+
+			categoryQueryObj['status'] = statusCode["ACTIVE"];
+
+			service.findAllRows(categoryModel, includeArr, categoryQueryObj, categoryOffset, categoryLimit, categoryField, categoryOrder)
+				.then(function (category) {
+					var categories = category.rows;
+					bottomCategory['left'] = categories.slice(0, 8);
+					bottomCategory['right'] = categories.slice(8, 16);
+					return callback(null, category.rows);
+				}).catch(function (error) {
+					console.log('Error :::', error);
+					return callback(null);
+				});
+		},
+		inboxMail: function (callback) {
+
+			includeArray = [
+				{ 
+					"model": model['Mail'],
+					where : {
+						status : statusCode["ACTIVE"],
+						to_id : user_id	
+						   },
+					    include: [{
+							model: model['User'],
+							as: 'fromUser',
+							attributes: ['id', 'first_name']
+						}],
+				}];
+
+			service.findRows(mailModel, queryObj, offset, limit, field, order,includeArray)
+				.then(function (mail) {
+					return callback(null, mail);
+
+				}).catch(function (error) {
+					console.log('Error :::', error);
+					return callback(null);
+				});
+		}
+	},
+		function (err, results) {
 			if (!err) {
 				var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-            var dropDownUrl = fullUrl.replace(req.url, '').replace(req.protocol + '://' + req.get('host'), '').replace('/', '');
+				var dropDownUrl = fullUrl.replace(req.url, '').replace(req.protocol + '://' + req.get('host'), '').replace('/', '');
 				res.render('gtc-mail/inbox', {
 					title: "Global Trade Connect",
 					LoggedInUser: LoggedInUser,
 					categories: results.categories,
 					bottomCategory: bottomCategory,
-					cartheader:results.cartCounts,
-					selectedPage: 'inbox',
-					vendorPlan:vendorPlan,
-					dropDownUrl : dropDownUrl
+					cartheader: results.cartCounts,
+					inboxMail: results.inboxMail.rows,
+					collectionSize: results.inboxMail.count,
+					page: page,
+					pageSize: limit,
+					maxSize: 5,
+                    selectedPage: 'inbox',
+					vendorPlan: vendorPlan,
+					dropDownUrl: dropDownUrl
 				});
 			} else {
 				res.render('gtc-mail/inbox', err);
@@ -74,56 +133,77 @@ export function inbox(req, res) {
 export function compose(req, res) {
 	var LoggedInUser = {};
 	var bottomCategory = {};
-	
+	var userMOdel = 'User';
+	var offset, limit, field, order;
+	var queryObj = {};
+
+	offset = 0;
+	limit = null;
+	field = "id";
+	order = "asc";
+
 	if (req.user)
 		LoggedInUser = req.user;
 
 	let user_id = LoggedInUser.id;
 	async.series({
-		cartCounts: function(callback) {
-            service.cartHeader(LoggedInUser).then(function(response) {
-                return callback(null, response);
-            }).catch(function(error) {
-                console.log('Error :::', error);
-                return callback(null);
-            });
-        },
-			categories: function(callback) {
-				var includeArr = [];
-				const categoryOffset = 0;
-				const categoryLimit = null;
-				const categoryField = "id";
-				const categoryOrder = "asc";
-				var categoryModel = "Category";
-				const categoryQueryObj = {};
-
-				categoryQueryObj['status'] = statusCode["ACTIVE"];
-
-				service.findAllRows(categoryModel, includeArr, categoryQueryObj, categoryOffset, categoryLimit, categoryField, categoryOrder)
-					.then(function(category) {
-						var categories = category.rows;
-						bottomCategory['left'] = categories.slice(0, 8);
-						bottomCategory['right'] = categories.slice(8, 16);
-						return callback(null, category.rows);
-					}).catch(function(error) {
-						console.log('Error :::', error);
-						return callback(null);
-					});
-			}
+		cartCounts: function (callback) {
+			service.cartHeader(LoggedInUser).then(function (response) {
+				return callback(null, response);
+			}).catch(function (error) {
+				console.log('Error :::', error);
+				return callback(null);
+			});
 		},
-		function(err, results) {
+		categories: function (callback) {
+			var includeArr = [];
+			const categoryOffset = 0;
+			const categoryLimit = null;
+			const categoryField = "id";
+			const categoryOrder = "asc";
+			var categoryModel = "Category";
+			const categoryQueryObj = {};
+
+			categoryQueryObj['status'] = statusCode["ACTIVE"];
+
+			service.findAllRows(categoryModel, includeArr, categoryQueryObj, categoryOffset, categoryLimit, categoryField, categoryOrder)
+				.then(function (category) {
+					var categories = category.rows;
+					bottomCategory['left'] = categories.slice(0, 8);
+					bottomCategory['right'] = categories.slice(8, 16);
+					return callback(null, category.rows);
+				}).catch(function (error) {
+					console.log('Error :::', error);
+					return callback(null);
+				});
+		},
+		users: function (callback) {
+			service.findRows(userMOdel, queryObj, offset, limit, field, order)
+				.then(function (users) {
+
+					console.log("=========================================", users.rows);
+					return callback(null, users.rows);
+
+				}).catch(function (error) {
+					console.log('Error :::', error);
+					return callback(null);
+				});
+		}
+	},
+		function (err, results) {
 			if (!err) {
 				var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-            var dropDownUrl = fullUrl.replace(req.url, '').replace(req.protocol + '://' + req.get('host'), '').replace('/', '');
+				var dropDownUrl = fullUrl.replace(req.url, '').replace(req.protocol + '://' + req.get('host'), '').replace('/', '');
 				res.render('gtc-mail/compose', {
 					title: "Global Trade Connect",
 					LoggedInUser: LoggedInUser,
 					categories: results.categories,
 					bottomCategory: bottomCategory,
-					cartheader:results.cartCounts,
+					cartheader: results.cartCounts,
+					users: results.users,
 					selectedPage: 'inbox',
-					vendorPlan:vendorPlan,
-					dropDownUrl : dropDownUrl
+					vendorPlan: vendorPlan,
+					dropDownUrl: dropDownUrl
 				});
 			} else {
 				res.render('gtc-mail/compose', err);
