cef1e61f8076fad66c7ce8831bd6375a746f0a37
rsumithra <sumiraja28@gmail.com>
2018-08-20 19:49:16 +0530

    sumithra:ligthbox

---
+++ server/assets/plugins/magnific-popup/custom-magnific-popup.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		focus: false,
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+		}
+	});
+	$('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		focus: false,
+		image: {
+			verticalFit: true
+		}
+		
+	});
+});
