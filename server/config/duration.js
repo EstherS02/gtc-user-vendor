25ceea22b30791664528f0b578febd6ea6ace0a6
nveera <veeramc94@gmail.com>
2018-08-30 20:40:08 +0530

    Ui changes in Search Page

---
+++ server/config/duration.js
@@ -0,0 +1,62 @@
+module.exports = [{
+    "duration": "Less than 1 Week",
+    "duration_type": "DAY",
+    "innerDuration": [{
+        "label": "1 Day Before",
+        "value": 1
+    }, {
+        "label": "2 Days Before",
+        "value": 2
+    }, {
+        "label": "3 Days Before",
+        "value": 3
+    }, {
+        "label": "4 Days Before",
+        "value": 4
+    }, {
+        "label": "5 Days Before",
+        "value": 5
+    }, {
+        "label": "6 Days Before",
+        "value": 6
+    }, {
+        "label": "7 Days Before",
+        "value": 7
+    }]
+}, {
+    "duration": "Less than 1 Month",
+    "duration_type": "WEEK",
+    "innerDuration": [{
+        "label": "1 Week Before",
+        "value": 1
+    }, {
+        "label": "2 Weeks Before",
+        "value": 2
+    }, {
+        "label": "3 Weeks Before",
+        "value": 3
+    }, {
+        "label": "4 Weeks Before",
+        "value": 4
+    }]
+}, {
+    "duration": "Monthly",
+    "duration_type": "MONTH",
+    "innerDuration": [{
+        "label": "January",
+        "value": 0
+    }, {
+        "label": "February",
+        "value": 1
+    }]
+}, {
+    "duration": "Yearly",
+    "duration_type": "YEAR",
+    "innerDuration": [{
+        "label": "2018",
+        "value": 2018
+    }, {
+        "label": "2019",
+        "value": 2019
+    }]
+}]
