007142e7b3679174e1912209116ceffa2b6413c4
rsumithra <sumiraja28@gmail.com>
2018-09-01 15:45:58 +0530

    sumithra:notification read and unread

--- server/assets/plugins/magnific-popup/custom-magnific-popup.js
+++ server/assets/plugins/magnific-popup/custom-magnific-popup.js
@@ -39,4 +39,14 @@ $(document).ready(function() {
 		removalDelay: 300,
 		mainClass: 'my-mfp-zoom-in'
 	});
+	$('.popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		modal: true
+	});
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
 });
