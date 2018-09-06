13622161588004b94b7de39c9e89db7e99f0dce6
krbalaji <krbalaji@ibcl3101>
2018-09-06 17:49:05 +0530

    chat changes UI

--- server/hbs-routes/messages/messages.controller.js
+++ server/hbs-routes/messages/messages.controller.js
@@ -62,7 +62,7 @@ export function messages(req, res) {
 							[model['TalkThread'],model['Talk'], "id", "desc"]
 						]
 					}).then(function(results1) {
-						return callback(null,results1);
+			 			return callback(null,results1);
 					}).catch(function(error) {
 						console.log('Error :::', error);
 						return callback(null);
@@ -89,7 +89,7 @@ export function messages(req, res) {
 						console.log('Error :::', error);
 						return callback(null);
 					});
-			},
+			}
 		},
 		function(err, results) {
 
