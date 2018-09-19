a33ba4304e207f962f169a2cd96a1c2daaf464ef
Esther <estherpushpa04@gmail.com>
2018-09-19 11:52:36 +0530

    stripe disconnect..

--- server/api/stripe/index.js
+++ server/api/stripe/index.js
@@ -8,6 +8,7 @@ var permission = require('../../config/permission');
 var router = express.Router();
 
 router.get('/connect', auth.isAuthenticated(), controller.stripeConnect);
+router.post('/disconnect', auth.isAuthenticated(), controller.stripeDisconnect);
 
 module.exports = router;
 
