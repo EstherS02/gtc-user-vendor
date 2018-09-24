3ce9d9d62640ca798228be5685e2ca291dd92b79
mchandirasekar <mchandru@ibytecode.com>
2018-09-24 15:56:44 +0530

    Cart Info method added all page without checkout and cart pages

--- server/hbs-routes/verification/verification.controller.js
+++ server/hbs-routes/verification/verification.controller.js
@@ -5,6 +5,8 @@ const model = require('../../sqldb/model-connect');
 const reference = require('../../config/model-reference');
 const statusCode = require('../../config/status');
 const service = require('../../api/service');
+const marketplace = require('../../config/marketplace');
+const cartService = require('../../api/cart/cart.service');
 const sequelize = require('sequelize');
 const moment = require('moment');
 import series from 'async/series';
@@ -13,12 +15,12 @@ const verificationStatus = require('../../config/verification_status');
 const vendorPlan = require('../../config/gtc-plan');
 
 export function verification(req, res) {
-    var LoggedInUser = {};
-    var bottomCategory ={};
+	var LoggedInUser = {};
+	var bottomCategory = {};
 
-    if(req.user)
-    LoggedInUser = req.user;
-    let user_id = LoggedInUser.id;
+	if (req.user)
+		LoggedInUser = req.user;
+	let user_id = LoggedInUser.id;
 
 	var modelName = "VendorVerification";
 	var queryObj = {};
@@ -27,27 +29,30 @@ export function verification(req, res) {
 
 
 	queryObj = {
-		vendor_id :vendor_id
+		vendor_id: vendor_id
 	};
 	var includeArr = [];
 
-    	async.series({
-    		cartCounts: function(callback) {
-            service.cartHeader(LoggedInUser).then(function(response) {
-                return callback(null, response);
-            }).catch(function(error) {
-                console.log('Error :::', error);
-                return callback(null);
-            });
-        },
+	async.series({
+			cartInfo: function(callback) {
+				if (LoggedInUser.id) {
+					cartService.cartCalculation(LoggedInUser.id)
+						.then((cartResult) => {
+							return callback(null, cartResult);
+						}).catch((error) => {
+							return callback(error);
+						});
+				} else {
+					return callback(null);
+				}
+			},
 			verification: function(callback) {
 				service.findOneRow(modelName, queryObj, includeArr)
 					.then(function(response) {
-						if(response){
-						return callback(null, response);
-						}else
-						{
-							return callback(null,null);
+						if (response) {
+							return callback(null, response);
+						} else {
+							return callback(null, null);
 						}
 					}).catch(function(error) {
 						console.log('Error :::', error);
@@ -55,28 +60,28 @@ export function verification(req, res) {
 					});
 			},
 			categories: function(callback) {
-            var includeArr = [];
-            const categoryOffset = 0;
-            const categoryLimit = null;
-            const categoryField = "id";
-            const categoryOrder = "asc";
-            var categoryModel = "Category";
-            const categoryQueryObj = {};
+				var includeArr = [];
+				const categoryOffset = 0;
+				const categoryLimit = null;
+				const categoryField = "id";
+				const categoryOrder = "asc";
+				var categoryModel = "Category";
+				const categoryQueryObj = {};
 
-            categoryQueryObj['status'] = statusCode["ACTIVE"];
+				categoryQueryObj['status'] = statusCode["ACTIVE"];
 
-            service.findAllRows(categoryModel, includeArr, categoryQueryObj, categoryOffset, categoryLimit, categoryField, categoryOrder)
-                .then(function(category) {
-                    var categories = category.rows;
-                    bottomCategory['left'] = categories.slice(0, 8);
-                    bottomCategory['right'] = categories.slice(8, 16);
-                    return callback(null, category.rows);
-                }).catch(function(error) {
-                    console.log('Error :::', error);
-                    return callback(null);
-                });
+				service.findAllRows(categoryModel, includeArr, categoryQueryObj, categoryOffset, categoryLimit, categoryField, categoryOrder)
+					.then(function(category) {
+						var categories = category.rows;
+						bottomCategory['left'] = categories.slice(0, 8);
+						bottomCategory['right'] = categories.slice(8, 16);
+						return callback(null, category.rows);
+					}).catch(function(error) {
+						console.log('Error :::', error);
+						return callback(null);
+					});
 
-        }	
+			}
 		},
 		function(err, results) {
 			if (!err) {
@@ -84,12 +89,13 @@ export function verification(req, res) {
 					title: "Global Trade Connect",
 					verification: results.verification,
 					LoggedInUser: LoggedInUser,
-					status:statusCode,
+					status: statusCode,
 					categories: results.categories,
-                	bottomCategory: bottomCategory,
+					bottomCategory: bottomCategory,
 					verificationStatus: verificationStatus,
-					cartheader: results.cartCounts,
-					vendorPlan:vendorPlan
+					cart: results.cartInfo,
+					marketPlace: marketplace,
+					vendorPlan: vendorPlan
 				});
 			} else {
 				res.render('vendorNav/verification', err);
