02336fd4d4c595902688a7defa20c1e38ff0dc76
nveera <veeramc94@gmail.com>
2018-09-24 17:47:37 +0530

    bug no 218 resolved

--- server/hbs-routes/user-verify/user-verify.controller.js
+++ server/hbs-routes/user-verify/user-verify.controller.js
@@ -11,36 +11,52 @@ const populate = require('../../utilities/populate');
 
 export function userVerify(req, res) {
 	var bottomCategory = {};
+	var LoggedInUser = {};
+
+	if (req.gtcGlobalUserObj && req.gtcGlobalUserObj.isAvailable) {
+		LoggedInUser = req.gtcGlobalUserObj;
+	}
+
 	async.series({
-			categories: function(callback) {
-				var includeArr = [];
-				const categoryOffset = 0;
-				const categoryLimit = null;
-				const categoryField = "id";
-				const categoryOrder = "asc";
-				var categoryModel = "Category";
-				const categoryQueryObj = {};
-
-				categoryQueryObj['status'] = status["ACTIVE"];
-
-				service.findAllRows(categoryModel, includeArr, categoryQueryObj, categoryOffset, categoryLimit, categoryField, categoryOrder)
-					.then(function(category) {
-						var categories = category.rows;
-						bottomCategory['left'] = categories.slice(0, 8);
-						bottomCategory['right'] = categories.slice(8, 16);
-						return callback(null, category.rows);
-					}).catch(function(error) {
-						console.log('Error :::', error);
-						return callback(null);
-					});
-			}
+		cartCounts: function(callback) {
+			service.cartHeader(LoggedInUser).then(function(response) {
+				return callback(null, response);
+			}).catch(function(error) {
+				console.log('Error :::', error);
+				return callback(null);
+			});
 		},
+		categories: function(callback) {
+			var includeArr = [];
+			const categoryOffset = 0;
+			const categoryLimit = null;
+			const categoryField = "id";
+			const categoryOrder = "asc";
+			var categoryModel = "Category";
+			const categoryQueryObj = {};
+
+			categoryQueryObj['status'] = status["ACTIVE"];
+
+			service.findAllRows(categoryModel, includeArr, categoryQueryObj, categoryOffset, categoryLimit, categoryField, categoryOrder)
+				.then(function(category) {
+					var categories = category.rows;
+					bottomCategory['left'] = categories.slice(0, 8);
+					bottomCategory['right'] = categories.slice(8, 16);
+					return callback(null, category.rows);
+				}).catch(function(error) {
+					console.log('Error :::', error);
+					return callback(null);
+				});
+		}
+	},
 		function(err, results) {
 			if (!err) {
 				res.render('user-verify', {
 					title: "Global Trade Connect",
 					categories: results.categories,
 					bottomCategory: bottomCategory,
+					LoggedInUser: LoggedInUser,
+					cartheader: results.cartCounts
 				});
 			} else {
 				res.render('users/user-join', err);
