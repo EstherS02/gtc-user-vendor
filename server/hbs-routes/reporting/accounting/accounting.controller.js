bcd0af146f51e6c93e9ee5b13820c961512567fd
mchandirasekar <mchandru@ibytecode.com>
2018-09-29 20:07:33 +0530

    accounting date range

--- server/hbs-routes/reporting/accounting/accounting.controller.js
+++ server/hbs-routes/reporting/accounting/accounting.controller.js
@@ -3,6 +3,7 @@
 var async = require('async');
 const moment = require('moment');
 const sequelize = require('sequelize');
+const querystring = require('querystring');
 
 const config = require('../../../config/environment');
 const model = require('../../../sqldb/model-connect');
@@ -16,10 +17,50 @@ const cartService = require('../../../api/cart/cart.service');
 const marketplace = require('../../../config/marketplace');
 
 export function accounting(req, res) {
-	var queryURI = {};
+	var queryParams = {};
 	var bottomCategory = {};
 	var LoggedInUser = req.user;
 	var categoryModel = "Category";
+	const dateRangeOptions = [{
+		"column": "Today",
+		"value": 1
+	}, {
+		"column": "Yesterday",
+		"value": 2
+	}, {
+		"column": "Last 7 Days",
+		"value": 3
+	}, {
+		"column": "Last 30 Days",
+		"value": 4
+	}, {
+		"column": "This Month",
+		"value": 5
+	}, {
+		"column": "Last Month",
+		"value": 6
+	}, {
+		"column": "Custom Range",
+		"value": 7
+	}];
+
+	if (req.query.range) {
+		queryParams['range'] = req.query.range;
+	} else {
+		queryParams['range'] = 4;
+	}
+
+	if (req.query.start_date) {
+		queryParams['start_date'] = req.query.start_date;
+	} else {
+		queryParams['start_date'] = moment().subtract(30, 'days').format('DD-MM-YYYY');
+	}
+
+	if (req.query.end_date) {
+		queryParams['end_date'] = req.query.end_date;
+	} else {
+		queryParams['end_date'] = moment().subtract(1, 'days').format('DD-MM-YYYY');
+	}
 
 	async.series({
 		cartInfo: function(callback) {
@@ -59,6 +100,7 @@ export function accounting(req, res) {
 				title: "Global Trade Connect",
 				category: results.category,
 				selectedPage: 'accounting',
+				categories: results.categories,
 				bottomCategory: bottomCategory,
 				selectedPage: 'accounting',
 				orderStatus: orderStatus,
@@ -67,7 +109,10 @@ export function accounting(req, res) {
 				dropDownUrl: dropDownUrl,
 				cart: results.cartInfo,
 				marketPlace: marketplace,
-				statusCode: statusCode
+				statusCode: statusCode,
+				queryParams: queryParams,
+				queryURI: querystring.stringify(queryParams),
+				dateRangeOptions: dateRangeOptions
 			});
 		} else {
 			return res.render('vendorNav/reporting/accounting', error);
