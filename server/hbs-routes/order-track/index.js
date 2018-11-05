0748e927ec7335b4cac76a12bcf89e89418928de
M Chandira sekar <mchandru@ibytecode.com>
2018-11-05 13:41:02 +0530

    reverted commit

--- server/hbs-routes/order-track/index.js
+++ server/hbs-routes/order-track/index.js
@@ -3,10 +3,10 @@
 var express = require('express');
 var router = express.Router();
 var auth = require('../../auth/auth.service');
-const roles = require('../../config/roles');
+
 var controller = require('./order-track.controller');
 
-router.get('/:id',auth.hasRole(roles['USER']), controller.orderTrack);
-router.get('/',auth.hasRole(roles['USER']), controller.orderTrack);
+router.get('/:id',auth.isAuthenticated(), controller.orderTrack);
+router.get('/',auth.isAuthenticated(), controller.orderTrack);
 
 module.exports = router;
