70ce1e47287abf658ca1ca3b6d0c38c717981edb
vnagalakshmi <nagavenkatv@gmail.com>
2018-11-13 12:04:52 +0530

    added the counts for groducts

--- server/api/product/product.service.js
+++ server/api/product/product.service.js
@@ -899,6 +899,24 @@ export function compareProducts(params) {
 	});
 }
 
+export function productGlobalCounts(params) {
+	console.log("enter the llliiiiiiiiiiiiiiiii");
+	return new Promise((resolve, reject) => {
+		if (params) {
+			Sequelize_Instance.query(RawQueries.productGlobalCountsQuery(params), {
+				model: model['product'],
+				type: Sequelize_Instance.QueryTypes.SELECT
+			}).then((results) => {
+				resolve(results)
+			}).catch(function(error) {
+				reject(error);
+			});
+		} else {
+			resolve()
+		}
+	});
+}
+
 function string_to_slug(str) {
 	str = str.replace(/^\s+|\s+$/g, ''); // trim
 	str = str.toLowerCase();
