fac98b422157d86956f52df10ac2ecf2af5bf748
svdevan <svdevan@ibytecode.com>
2018-11-26 17:52:23 +0530

    create admin endpoint opened without auth

--- server/api/admin/index.js
+++ server/api/admin/index.js
@@ -8,7 +8,9 @@ const roles = require('../../config/roles');
 
 router.get('/me', auth.hasRole(roles['ADMIN']), controller.me);
 router.get('/', auth.hasRole(roles['ADMIN']), controller.index);
-router.post('/', auth.hasRole(roles['ADMIN']), controller.create);
-router.put('/:id',auth.isAuthenticated(), controller.edit);
+router.post('/', controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.edit);
+
+//auth.hasRole(roles['ADMIN'])
 
 module.exports = router;
