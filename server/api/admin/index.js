37dbf5e253b97f8cee8f70882c885753a70387fe
svdevan <svdevan@ibytecode.com>
2018-11-26 18:06:16 +0530

    closed unauthenticated admin endpoint

--- server/api/admin/index.js
+++ server/api/admin/index.js
@@ -8,9 +8,7 @@ const roles = require('../../config/roles');
 
 router.get('/me', auth.hasRole(roles['ADMIN']), controller.me);
 router.get('/', auth.hasRole(roles['ADMIN']), controller.index);
-router.post('/', controller.create);
+router.post('/', auth.hasRole(roles['ADMIN']), controller.create);
 router.put('/:id', auth.isAuthenticated(), controller.edit);
-
-//auth.hasRole(roles['ADMIN'])
 
 module.exports = router;
