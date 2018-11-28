3e04190238c0666cf6198be8b518847920f88603
raviteja <csomethingr@gmail.com>
2018-11-28 15:41:58 +0530

    faq supprate page

--- server/routes.js
+++ server/routes.js
@@ -92,6 +92,7 @@ export default function(app) {
     app.use('/order-checkout', require('./hbs-routes/checkout'));
     app.use('/user-join', require('./hbs-routes/user-join'));
     app.use('/web-rtc', require('./hbs-routes/web-rtc'));
+    app.use('/faq', require('./hbs-routes/faq'));
 
     app.use('/:vendorType(directories|sellers|wholesalers|retailers|services-providers|subscription-providers)', require('./hbs-routes/vendor-search-result'));
     //should be last route
