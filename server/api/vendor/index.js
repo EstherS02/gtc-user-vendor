d2d349856632835ecc9d51ff8299102ae40e37c8
Esther <estherpushpa04@gmail.com>
2018-11-28 19:52:25 +0530

    create vendor api for admin..

--- server/api/vendor/index.js
+++ server/api/vendor/index.js
@@ -11,7 +11,7 @@ var controller = require('./vendor.controller');
 router.get('/me', auth.isAuthenticated(), controller.me);
 router.get('/',auth.hasRole(roles['ADMIN']),controller.index);
 router.post('/delete',auth.hasRole(roles['ADMIN']),controller.deleteVendor);
-router.post('/create-vendor', auth.hasRole(roles['USER']), auth.isEmailVerified(), auth.isAccountActive(), multipartMiddleware, controller.createVendor);
+router.post('/create-vendor', auth.hasRole(roles['ADMIN']), auth.isEmailVerified(), auth.isAccountActive(), multipartMiddleware, controller.createVendor);
 router.post('/starter-seller', auth.hasRole(roles['USER']), auth.isEmailVerified(), auth.isAccountActive(), multipartMiddleware, controller.createStarterSeller);
 router.post('/',auth.hasRole(roles['VENDOR']), controller.create);
 router.put('/',auth.hasRole(roles['ADMIN']),controller.deleteVendor)
