9e0063c48ff39a190bbc3f29f141c0a2c883aaa8
M Chandira sekar <mchandru@ibytecode.com>
2018-12-01 17:02:49 +0530

    payment config changed

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -143,10 +143,10 @@ var all = {
 		lifestyle_fee: 10
 	},
 	payment: {
-		"cancelOrderItem": 0,
-		"returnOrderItem": 0,
-		"noResposeOrderItem": 0,
-		"vendorPayout": 0
+		"cancelOrderItem": 3,
+		"returnOrderItem": 3,
+		"noResposeOrderItem": 3,
+		"vendorPayout": 30
 	},
 	sesTransporter: {
 		accessKeyId: process.env.SES_ACCESS_KEY_ID,
