9e3235c9576f602b3df1180415a405b8d3a1283f
vnagalakshmi <nagavenkatv@gmail.com>
2019-02-28 12:02:19 +0530

    added the export functionality

--- server/api/export-csv/index.js
+++ server/api/export-csv/index.js
@@ -9,12 +9,7 @@ router.get('/orders-csv', auth.isAuthenticated(),controller.myOrderHistoryexport
 router.get('/personal-orders-csv', auth.isAuthenticated(),controller.orderHistoryexportcsv);
 router.get('/listings-csv',auth.isAuthenticated(), controller.exportcsv);
 router.post('/saleshistorycsv',auth.isAuthenticated(), controller.salesHistoryexportcsv);
-router.get('/export-vendors-performance',auth.isAuthenticated(), controller.vendorPerformancecsv);
+// router.get('/export-vendors-performance',auth.isAuthenticated(), controller.vendorPerformancecsv);
 router.get('/export-product-performance',auth.isAuthenticated(), controller.productPerformanceChangescsv);
-router.get('/export-category-performance',auth.isAuthenticated(), controller.compareCategoryPerformancecsv);
-router.get('/export-marketplace-performance',auth.isAuthenticated(), controller.compareMarketPlacePerformancecsv);
-router.get('/export-city-performance',auth.isAuthenticated(), controller.compareCityPerformancecsv);
-router.get('/export-country-performance',auth.isAuthenticated(), controller.compareCountriesPerformancecsv);
-router.get('/export-user-performance',auth.isAuthenticated(), controller.compareUserPerformancecsv);
 
 module.exports = router;
